cd0000b473cb1c27455a2603ced33e70cdf74445
Rob Monday <rob.monday@gmail.com>
2016-04-10 23:44:12 -0500

    resynch

---
+++ vagrant/tournament/add_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO players (name, team_color1, team_color2) VALUES ('Duke','Dark Blue','White');
+INSERT INTO players (name, team_color1, team_color2) VALUES ('UNC','Light Blue','White');
+INSERT INTO players (name, team_color1, team_color2) VALUES ('Harvard','Crimson','White');
+INSERT INTO players (name, team_color1, team_color2) VALUES ('Tennessee','Light Orange','White');
+INSERT INTO players (name, team_color1, team_color2) VALUES ('Clemson','Orange','Purple');
+INSERT INTO players (name, team_color1, team_color2) VALUES ('Vanderbilt','Black', 'Gold');
+
+INSERT INTO matches (round, player1, player2, winner, loser) VALUES (1,'Clemson','Vanderbilt','Clemson','Vanderbilt');
+INSERT INTO matches (round, player1, player2, winner, loser) VALUES (1,'Duke','UNC','Duke','UNC');
+INSERT INTO matches (round, player1, player2, winner, loser) VALUES (1,'Tennessee', 'Harvard', 'Tennessee','Harvard');
+
+
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -2,8 +2,27 @@
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
 -- statements if you choose to use it.
---
--- You can write comments in this file by starting them with two dashes, like
--- these lines here.
 
+\c postgres;
+DROP DATABASE tournament; --prevents error from creating table that already exists
+CREATE DATABASE tournament;
+\c tournament;
+
+CREATE TABLE Players (
+	name 		varchar(32) PRIMARY KEY, 
+	team_color1	varchar(32),
+	team_color2 varchar(32)
+);
+
+CREATE TABLE Matches(
+	id serial,
+	tournament 	varchar(32) DEFAULT 'Current',
+	round 		int NOT NULL CHECK (round > 0),
+	game_time 	timestamp DEFAULT current_timestamp,
+	player1 	varchar(32) NOT NULL REFERENCES Players(name),
+	player2 	varchar(32) NOT NULL REFERENCES Players(name),
+	winner 		varchar(32) NOT NULL REFERENCES Players(name), 
+	loser 		varchar(32) NOT NULL REFERENCES Players(name), 
+	PRIMARY KEY (tournament, round, player1, player2)
+);
 
